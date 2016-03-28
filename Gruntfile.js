b0f75afe2bd7804ea0485bfe13cf36bd3cd2be10
Bryan Moore <bryan.moore64@gmail.com>
2016-03-28 22:04:38 +0200

    grunt less, cssmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -81,19 +81,85 @@ module.exports = function(grunt) {
 				    }()
 				}
 			}
+    },
+    less:{
+      build:
+			{
+			  options:
+				{
+				    paths: null,
+				    compress: false,
+				    ieCompat: true,
+				    optimization: null, //number
+				    strictImports: false,
+				    strictMath: false,
+				    strictUnits: false,
+				    syncImport: false,
+				    dumpLineNumbers: false,
+				    relativeUrls: false,
+				    sourceMap: false,
+				    outputSourceFiles: false
+				},
+			  files:
+				{
+          "ui/styles/css/global.css": "ui/styles/less/global.less",
+			    "ui/styles/css/blocks.css": "ui/styles/less/blocks.less",
+			    "ui/styles/css/pages.css": "ui/styles/less/pagetypes.less",
+			    "ui/styles/css/icons.css": "ui/styles/less/resources.less",
+				}
+			}
+    },
+    cssmin:{
+      options:
+			{
+			    report: "min",					// min, gzip (slower)
+			    advanced: true,					//set to false to disable advanced optimizations - selector & property merging, reduction, etc.
+			    aggressiveMerging: true,		//set to false to disable aggressive merging of properties
+			    debug: false,					// set to true to get minification statistics under stats property (see test/custom-test.js for examples)
+			    keepBreaks: false,				// whether to keep line breaks (default is false)
+			    keepSpecialComments: '0',		//* for keeping all (default), 1 for keeping first one only, 0 for removing all
+			    processImport: true,			// whether to process @import rules
+			    rebase: false,					// set to false to skip URL rebasing
+			    relativeTo: '',					// path to resolve relative @import rules and URLs
+			    root: '',						// path to resolve absolute @import rules and rebase relative URLs
+			    roundingPrecision: -1,			// rounding precision; defaults to 2; -1 disables rounding
+			    shorthandCompacting: true,		// set to false to skip shorthand compacting (default is true unless sourceMap is set when it's false)
+			    sourceMap: false,				// exposes source map under sourceMap property, e.g. new CleanCSS().minify(source).sourceMap (default is false) If input styles are a product of CSS preprocessor (LESS, SASS) an input source map can be passed as a string.
+			    target: '',						// path to a folder or an output file to which rebase all URLs
+			},
+		  sitefiles:
+			{
+			    files:
+				{
+				    'ui/styles/main.min.css':
+					[
+						'ui/styles/css/pure.css',
+						'ui/styles/css/grids-responsive.css',
+            'ui/styles/css/global.css',
+            'ui/styles/css/icons.css',
+						'ui/styles/css/forms.css',
+            'ui/styles/css/tables.css',
+						'ui/styles/css/pagetypes.css',
+						'ui/styles/css/blocks.css',
+            'ui/styles/css/owl.carousel.css',
+            'ui/styles/css/owl.theme.css',
+					]
+				}
+			}
     }
   });
 
   //load tasks
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-requirejs');
-  //grunt.loadNpmTasks('grunt-contrib-less');
-  //grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-ts');
 
   //define tasks
-  grunt.registerTask('default', ['ts']);
+  grunt.registerTask('default','ts');
   grunt.registerTask('static','uglify:sitefiles');
   grunt.registerTask('require','requirejs');
+  grunt.registerTask('styles',['less','cssmin']);
 };
